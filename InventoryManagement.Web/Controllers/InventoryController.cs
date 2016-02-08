c1497d95eb21d63be5a152d0af91500d870cd3d8
Abul Wahab <ab.1303@gmail.com>
2016-02-09 02:00:59 +0500

    Make Inventory

--- InventoryManagement.Web/Controllers/InventoryController.cs
+++ InventoryManagement.Web/Controllers/InventoryController.cs
@@ -23,31 +23,247 @@ namespace InventoryManagement.Web.Controllers
         // GET: Inventory
         public ActionResult Index()
         {
-            return View();
+            var form = new InventorySummary
+            {
+                ItemList =
+                      _context.Items.ToListItems(t => (t.ItemCode + " - " + t.ItemName),
+                          v => v.ItemId.ToString(CultureInfo.InvariantCulture), "Please Select...")
+            };
+
+            return View(form);
         }
 
+        public JsonResult MakeInventory(int itemId)
+        {
+            string message;
+            try
+            {
+                var item = _context.Items.SingleOrDefault(i => i.ItemId == itemId);
+                if (item == null)
+                {
+                    message = "Unexpected Error!!";
+                      return Json(
+                          new{ 
+                              InvetorySummary = 
+                               new
+                               {
+                                   ItemName = "",
+                                   ItemCode="",
+                                   NumberOfPieces = -1,
+                                   NumberOfCartons = -1,
+                                   TotalPieces = -1
+                               },
+                               Message = message
+                        });
+                }
+
+
+                var itemInventory = _context.Inventories.SingleOrDefault(i => i.Carton.ItemId == itemId);
+                if (itemInventory == null)
+                {
+                    //TODO: Divide into cartons and pieces
+                    message = "No Inventory defined!!";
+                    return Json(
+                        new
+                        {
+                            InvetorySummary =
+                             new
+                             {
+                                 ItemName = item.ItemName,
+                                 ItemCode = item.ItemCode,
+                                 NumberOfPieces = 0,
+                                 NumberOfCartons = 0,
+                                 TotalPieces = 0
+                             },
+                            Message = message
+                        });
+
+                }
+
+                var floatPieces = itemInventory.NumberOfPieces;
+                var totalCartons = itemInventory.NumberOfCartons;
+                var totalPieces = (totalCartons * itemInventory.Carton.NumberOfPieces) + floatPieces;
+                message = "Item Inventory made!";
+                return Json(
+                    new
+                    {
+                        InvetorySummary =
+                         new
+                         {
+                             ItemName = item.ItemName,
+                             ItemCode = item.ItemCode,
+                             NumberOfPieces = floatPieces,
+                             NumberOfCartons = totalCartons,
+                             TotalPieces = totalPieces
+                         },
+                        Message = message
+                    });
+
+
+            }
+            catch (Exception ex)
+            {
+                message = "Server Error making inventory!";
+                return Json(
+                         new
+                         {
+                             InvetorySummary = new {},
+                             Message = message
+                         });
+            }
+        }
+
+
+        # region "Item Inventory"
         public ActionResult AddPiece()
         {
-            return View();
+            var form = new ItemInventory
+            {
+                ItemList =
+                      _context.Items.ToListItems(t => (t.ItemCode + " - " + t.ItemName),
+                          v => v.ItemId.ToString(CultureInfo.InvariantCulture), "Please Select...")
+            };
+
+            return View(form);
         }
 
         [HttpPost, ValidateAntiForgeryToken, Log("Add Piece")]
         public ActionResult AddPiece(ItemInventory formItemInventory)
         {
-            return View();
+            try
+            {
+                var itemInventory = _context.Inventories.SingleOrDefault(i => i.Carton.ItemId == formItemInventory.ItemId);
+                if (itemInventory == null)
+                {
+
+                    //TODO: Divide into cartons and pieces
+                    var carton = _context.Cartons.SingleOrDefault(i => i.ItemId == formItemInventory.ItemId);
+                    if (carton == null)
+                    {
+                        //TODO: Need to think about a TPH Strategy; throw error for now
+                        return RedirectToAction<InventoryController>(c => c.Index())
+                            .WithError("No carton defined for respective Item");
+                    }
+
+                    int numberOfCartons;
+                    int floatPieces;
+                    DivideIntoLotSize(formItemInventory.NumberOfPieces, carton.NumberOfPieces, out numberOfCartons,
+                        out floatPieces);
+
+                    _context.Inventories.Add(new Inventory
+                    {
+                        CartonId = carton.CartonId,
+                        NumberOfCartons = numberOfCartons,
+                        NumberOfPieces = floatPieces
+                    });
+
+                }
+                else
+                {
+                    //TODO: Update
+                    var carton = _context.Cartons.SingleOrDefault(i => i.ItemId == formItemInventory.ItemId);
+
+                    if (carton == null)
+                    {
+                        //TODO: This shouldn't happen in business case
+                        return RedirectToAction<InventoryController>(c => c.Index())
+                            .WithError("Database Error, Contact Administrator");
+                    }
+
+                    var existingPieces = itemInventory.NumberOfPieces;
+                    var existingCartons = itemInventory.NumberOfCartons;
+
+                    int additionalNumberOfCartons;
+                    int floatPieces;
+                    DivideIntoLotSize(existingPieces + formItemInventory.NumberOfPieces,
+                        carton.NumberOfPieces, out additionalNumberOfCartons,
+                        out floatPieces);
+
+
+                    itemInventory.NumberOfCartons = existingCartons + additionalNumberOfCartons;
+                    itemInventory.NumberOfPieces = floatPieces;
+                }
+
+                _context.SaveChanges();
+
+                return RedirectToAction<InventoryController>(c => c.Index())
+                .WithSuccess("Piece(s) Added!");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction<InventoryController>(c => c.Index())
+                .WithError("Error Adding Piece(s)");
+            }
         }
 
         public ActionResult SellPiece()
         {
-            return View();
+            var form = new ItemInventory
+            {
+                ItemList =
+                      _context.Items.ToListItems(t => (t.ItemCode + " - " + t.ItemName),
+                          v => v.ItemId.ToString(CultureInfo.InvariantCulture), "Please Select...")
+            };
+
+            return View(form);
         }
 
         [HttpPost, ValidateAntiForgeryToken, Log("Sell Piece")]
         public ActionResult SellPiece(ItemInventory formItemInventory)
         {
-            return View();
+            try
+            {
+                var itemInventory = _context.Inventories.SingleOrDefault(i => i.Carton.ItemId == formItemInventory.ItemId);
+                if (itemInventory == null)
+                {
+                    //TODO: Divide into cartons and pieces
+                    return RedirectToAction<InventoryController>(c => c.Index())
+                         .WithWarning("No Inventory exists!");
+
+                }
+                //TODO: Update
+                var carton = _context.Cartons.SingleOrDefault(i => i.ItemId == formItemInventory.ItemId);
+
+                if (carton == null)
+                {
+                    //TODO: This shouldn't happen in business case
+                    return RedirectToAction<InventoryController>(c => c.Index())
+                        .WithError("Database Error, Contact Administrator");
+                }
+
+                var existingPieces = itemInventory.NumberOfPieces;
+                var existingCartons = itemInventory.NumberOfCartons;
+                var totalPieces = (existingCartons * carton.NumberOfPieces) + existingPieces;
+
+                if (totalPieces < formItemInventory.NumberOfPieces)
+                {
+                    return RedirectToAction<InventoryController>(c => c.Index())
+                       .WithWarning("Sell Request is more than total inventory");
+                }
+
+                int remainingNumberOfCartons;
+                int floatPieces;
+                DivideIntoLotSize(totalPieces - formItemInventory.NumberOfPieces,
+                    carton.NumberOfPieces, out remainingNumberOfCartons,
+                    out floatPieces);
+
+
+                itemInventory.NumberOfCartons = remainingNumberOfCartons;
+                itemInventory.NumberOfPieces = floatPieces;
+
+                _context.SaveChanges();
+
+                return RedirectToAction<InventoryController>(c => c.Index())
+                .WithSuccess("Piece(s) Sold!");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction<InventoryController>(c => c.Index())
+                .WithError("Error Selling Piece(s)");
+            }
         }
 
+        #endregion
 
         # region "Carton Inventory"
 
@@ -118,19 +334,19 @@ namespace InventoryManagement.Web.Controllers
         {
             try
             {
-                var carton = _context.Inventories.SingleOrDefault(i => i.CartonId == formCartonInventory.CartonId);
-                if (carton == null)
+                var cartonInvetory = _context.Inventories.SingleOrDefault(i => i.CartonId == formCartonInventory.CartonId);
+                if (cartonInvetory == null)
                 {
                     return RedirectToAction<InventoryController>(c => c.Index())
-                        .WithWarning("No Carton Inventory exists!");
+                        .WithWarning("No Inventory exists!");
 
                 }
 
                 //TODO: Update
-                var existingCartons = carton.NumberOfCartons;
+                var existingCartons = cartonInvetory.NumberOfCartons;
                 if (formCartonInventory.NumberOfCartons <= existingCartons)
                 {
-                    carton.NumberOfCartons = existingCartons - formCartonInventory.NumberOfCartons;
+                    cartonInvetory.NumberOfCartons = existingCartons - formCartonInventory.NumberOfCartons;
                 }
                 else
                 {
@@ -148,6 +364,16 @@ namespace InventoryManagement.Web.Controllers
                 return RedirectToAction<InventoryController>(c => c.Index())
                 .WithError("Error Selling Cartons");
             }
+        }
+
+        #endregion
+
+        #region "Utility Methods"
+
+        private void DivideIntoLotSize(int totalPieces, int cartonSize, out int numberOfCartons, out int numberOfPieces)
+        {
+            numberOfCartons = totalPieces / cartonSize;
+            numberOfPieces = totalPieces - (numberOfCartons * cartonSize);
         }
 
         #endregion
---
+++ InventoryManagement.Web/Models/Inventory/InventorySummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using System.Web.Mvc;
+using InventoryManagement.Web.Infrastructure.Mapping;
+
+namespace InventoryManagement.Web.Models.Inventory
+{
+    public class InventorySummary
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public IList<SelectListItem> ItemList { get; set; }
+        public int NumberOfPieces { get; set; }
+        public int NumberOfCartons { get; set; }
+        public int TotalPieces { get; set; }
+
+    }
+}
